d95681331a3c67c698bb67a50fc9d22ff8b5da2a
Josh Lucy <josh.lucy@legitscript.com>
2019-02-22 11:06:47 -0800

    First working version.

--- README.md
+++ README.md
@@ -1,26 +1,50 @@
 CZDS API Client in Python
 ===========
 
-This repository provides a Python example of how to download zone files via CZDS (Centralized Zone Data Service) REST API. 
+This repository provides a Python example of how to download zone files via CZDS (Centralized Zone Data Service) REST API.
 A detail API Specs can be found [here.](https://github.com/icann/czds-api-client-java/tree/master/docs)
 
-There is also an example provided in Java. It can be found in [this repo.](https://github.com/icann/czds-api-client-java)
+It has been modified to post CloudWatch metrics for each zone file successfully downloaded.
 
 Installation
 ------------
 
-This script requires Python 3. It has been tested with Python 3.7.1. 
+According to ICANN this script requires Python 3, but it works fine with Python 2.7. It has been tested with Python 3.7.1 and 2.7.12.
 
-It requires the `requests` extension library. Please checkout here to see how to install it - https://github.com/requests/requests
+It requires:
+* The `requests` extension library. It can be installed using `pip install requests`.
+* The `boto3` library. This can be installed using `pip install boto3`.
+
+AWS authentication
+------------------
+
+This utility supports authenticating with AWS in the following ways:
+* AWS instance profiles
+* AWS profiles defined in ~/.aws/credentials
+* AWS IAM keys
+
+AWS authentication parameters are configured in `config.json`. In order to use an instance profile ensure that `aws.iam.profile`, `aws.iam.access_key_id`, and `aws.iam.secret_access_key` are set to `null`. If you want to specify an instance profile just set `aws.iam.profile` to the name of the desired AWS profile you want to use. `aws.iam.access_key_id` and `aws.iam.secret_access_key` should be set to `null` and will be ignored in favor of the AWS authentication profile if specified. In order to use AWS access keys `aws.iam.profile` should be set to `null`.
+
+CloudWatch metrics
+------------------
+
+In order to track zone file download success this utility leverages CloudWatch metrics. Each zone file download attempt will generate a CloudWatch metric for the attempt. The CloudWatch namespace and region these metrics are stored with is configured `config.json` as `aws.coudwatch.namespace` and `aws.region` respectively. The dimensions for each zone file are as follows:
+* File: The name of the zone being downloaded.
+* Source: Statically set to `ICANN`.
+
+Breakdown of the `status` metric:
+* The absence of a `status` metric indicates the script didn't properly run or hasn't been executed.
+* A `status` metric of `0` indicates that there was an error during the download of the zone file.
+* A `status` metric of `1` indicates that the zone file downloaded successfully.
 
 Run
 ---------------------
 
-1. Make a copy of the `config.sample.json` file and name it `config.json`
+1. Copy the configuration file template to `config.json` by running `cp config.json{.dist,}`.
 2. Edit `config.json` and fill in your information.
 2. Run `python download.py`
 
-All the zone files will be saved in `working-directory`/zonefiles, `working-directory` is specified in `config.json`, 
+All the zone files will be saved in `working-directory`/zonefiles, `working-directory` is specified in `config.json`,
 or default to current directory if not specified in `config.json`
 
 Documentation
--- download.py
+++ download.py
@@ -1,29 +1,49 @@
-import json
-import sys
+import boto3
 import cgi
-import os
 import datetime
+import json
+import os
+import sys
+import traceback
 
 from do_authentication import authenticate
 from do_http_get import do_get
+
+aws_session_args = {}
+
 
 ##############################################################################################################
 # First Step: Get the config data from config.json file
 ##############################################################################################################
 
 try:
-    config_file = open("config.json", "r")
+    # In case we run the script from somewhere outside our working directory...
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    config_file = open(base_path + "/config.json", "r")
     config = json.load(config_file)
     config_file.close()
+
 except:
     sys.stderr.write("Error loading config.json file.\n")
     exit(1)
 
+# Front load some of the AWS creds values
+aws_profile           = None
+aws_access_key_id     = None
+aws_secret_access_key = None
+
 # The config.json file must contain the following data:
-username = config['icann.account.username']
-password = config['icann.account.password']
-authen_base_url = config['authentication.base.url']
-czds_base_url = config['czds.base.url']
+username                 = config['icann.account.username']
+password                 = config['icann.account.password']
+authentication_base_url  = config['authentication.base.url']
+czds_base_url            = config['czds.base.url']
+aws_region               = config['aws.region']
+aws_cloudwatch_namespace = config['aws.cloudwatch.namespace']
+
+# The config.json can contain the following data:
+aws_profile           = config['aws.iam.profile']
+aws_access_key_id     = config['aws.iam.access_key_id']
+aws_secret_access_key = config['aws.iam.secret_access_key']
 
 # This is optional. Default to current directory
 working_directory = config['working.directory']
@@ -36,7 +56,7 @@ if not password:
     sys.stderr.write("'icann.account.password' parameter not found in the config.json file\n")
     exit(1)
 
-if not authen_base_url:
+if not authentication_base_url:
     sys.stderr.write("'authentication.base.url' parameter not found in the config.json file\n")
     exit(1)
 
@@ -44,10 +64,33 @@ if not czds_base_url:
     sys.stderr.write("'czds.base.url' parameter not found in the config.json file\n")
     exit(1)
 
+if not aws_region:
+    sys.stderr.write("'aws.aws_region' parameter not found in the config.json file\n")
+    exit(1)
+
+if not aws_cloudwatch_namespace:
+    sys.stderr.write("'aws.cloudwatch.namespace' parameter not found in the config.json file\n")
+    exit(1)
+
 if not working_directory:
     # Default to current directory
     working_directory = '.'
 
+if (aws_access_key_id == None) ^ (aws_secret_access_key == None):
+    sys.stderr.write("'aws.iam.aws_access_key_id' and 'aws.iam.aws_secret_access_key' parameters must both be used together in config.json file\n")
+    exit(1)
+
+
+# Actually set up AWs creds if we're using keys or a profile. Otherwise we'll just
+# hope we're running on a host/container with an instance profile.
+if aws_access_key_id != None:
+    aws_session.update({
+        'aws_access_key_id': aws_access_key_id,
+        'aws_secret_access_key': aws_secret_access_key
+    })
+
+if aws_profile != None:
+    aws_session.update({'profile_name': aws_profile})
 
 
 ##############################################################################################################
@@ -56,8 +99,7 @@ if not working_directory:
 ##############################################################################################################
 
 print("Authenticate user {0}".format(username))
-access_token = authenticate(username, password, authen_base_url)
-
+access_token = authenticate(username, password, authentication_base_url)
 
 
 ##############################################################################################################
@@ -79,7 +121,7 @@ def get_zone_links(czds_base_url):
         return zone_links
     elif status_code == 401:
         print("The access_token has been expired. Re-authenticate user {0}".format(username))
-        access_token = authenticate(username, password, authen_base_url)
+        access_token = authenticate(username, password, authentication_base_url)
         get_zone_links(czds_base_url)
     else:
         sys.stderr.write("Failed to get zone links from {0} with error code {1}\n".format(links_url, status_code))
@@ -92,9 +134,16 @@ if not zone_links:
     exit(1)
 
 
+##############################################################################################################
+# Fourth Step: Set up AWS integration
+##############################################################################################################
+
+boto3.setup_default_session(**aws_session_args)
+aws_cloudwatch = boto3.client('cloudwatch', region_name=aws_region)
+
 
 ##############################################################################################################
-# Fourth Step: download zone files
+# Fifth Step: download zone files
 ##############################################################################################################
 
 # Function definition to download one zone file
@@ -102,39 +151,52 @@ def download_one_zone(url, output_directory):
     print("{0}: Downloading zone file from {1}".format(str(datetime.datetime.now()), url))
 
     global  access_token
-    download_zone_response = do_get(url, access_token)
 
-    status_code = download_zone_response.status_code
+    try:
+        download_zone_response = do_get(url, access_token)
 
-    if status_code == 200:
-        # Try to get the filename from the header
-        _,option = cgi.parse_header(download_zone_response.headers['content-disposition'])
-        filename = option['filename']
+        status_code = download_zone_response.status_code
 
-        # If could get a filename from the header, then makeup one like [tld].txt.gz
-        if not filename:
-            filename = url.rsplit('/', 1)[-1].rsplit('.')[-2] + '.txt.gz'
+        if status_code == 200:
+            # Try to get the filename from the header
+            _,option = cgi.parse_header(download_zone_response.headers['content-disposition'])
+            filename = option['filename']
 
-        # This is where the zone file will be saved
-        path = '{0}/{1}'.format(output_directory, filename)
+            # If could get a filename from the header, then makeup one like [tld].txt.gz
+            if not filename:
+                filename = url.rsplit('/', 1)[-1].rsplit('.')[-2] + '.txt.gz'
 
-        with open(path, 'wb') as f:
-            for chunk in download_zone_response.iter_content(1024):
-                f.write(chunk)
+            # This is where the zone file will be saved
+            path = '{0}/{1}'.format(output_directory, filename)
 
-        print("{0}: Completed downloading zone to file {1}".format(str(datetime.datetime.now()), path))
+            with open(path, 'wb') as f:
+                for chunk in download_zone_response.iter_content(1024):
+                    f.write(chunk)
 
-    elif status_code == 401:
-        print("The access_token has been expired. Re-authenticate user {0}".format(username))
-        access_token = authenticate(username, password, authen_base_url)
-        download_one_zone(url, output_directory)
-    elif status_code == 404:
-        print("No zone file found for {0}".format(url))
-    else:
-        sys.stderr.write('Failed to download zone from {0} with code {1}\n'.format(url, status_code))
+            print("{0}: Completed downloading zone to file {1}".format(str(datetime.datetime.now()), path))
+
+        elif status_code == 401:
+            print("The access_token has been expired. Re-authenticate user {0}".format(username))
+            access_token = authenticate(username, password, authentication_base_url)
+            download_one_zone(url, output_directory)
+        elif status_code == 404:
+            print("No zone file found for {0}".format(url))
+        else:
+            sys.stderr.write('Failed to download zone from {0} with code {1}\n'.format(url, status_code))
+
+        # Make sure we throw an exception we can catch back down the line.
+        if status_code >= 400:
+            raise RuntimeError("Download failed.")
+
+    except:
+        # Don't just die here. We need to know that we failed.
+        raise
 
 # Function definition for downloading all the zone files
 def download_zone_files(urls, working_directory):
+
+    # Set up a list to store our CloudWatch metrics.
+    cloudwatch_metrics = []
 
     # The zone files will be saved in a sub-directory
     output_directory = working_directory + "/zonefiles"
@@ -144,7 +206,42 @@ def download_zone_files(urls, working_directory):
 
     # Download the zone files one by one
     for link in urls:
-        download_one_zone(link, output_directory)
+        # Start with success.
+        cloudwatch_metric_value = 1
+
+        try:
+            # Taken from above download_one_zone().
+            filename = link.rsplit('/', 1)[-1].rsplit('.')[-2]
+
+            # This is where the zone file will be saved
+            download_one_zone(link, output_directory)
+
+        except:
+            # Failure happened.
+            cloudwatch_metric_value = 0
+            print("Exception downloading file:\n %s" %traceback.format_exc())
+
+        finally:
+            # Add the results to our list of results.
+            cloudwatch_metrics.append({
+                "MetricName": 'status',
+                "Value": cloudwatch_metric_value,
+                "Dimensions": [
+                    {
+                        "Name": "File",
+                        "Value": filename
+                    },
+                    {
+                        "Name": "Source",
+                        "Value": "ICANN"
+                    }
+                ],
+                "Timestamp": datetime.datetime.utcnow()
+            })
+
+    # Put all our metrics in one shot.
+    aws_cloudwatch.put_metric_data(Namespace=aws_cloudwatch_namespace, MetricData=cloudwatch_metrics)
+
 
 # Finally, download all zone files
 start_time = datetime.datetime.now()
